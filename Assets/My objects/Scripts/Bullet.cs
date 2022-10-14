f18bc77736f16cb90b15f16d31c5f597624c9be6
Daniel Hernandez <daniel_hernandez130999@hotmail.com>
2022-10-14 16:19:23 -0500

    Personaje puede apuntar y disparar, se creo el prefab bullet y su script

---
+++ Assets/My objects/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float speed = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 5);
+    }
+    void Update()
+    {
+        transform.position += transform.right.normalized * Time.deltaTime * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Que haga daño al enemigo
+    }
+}
--- Assets/My objects/Scripts/PlayerController.cs
+++ Assets/My objects/Scripts/PlayerController.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region Constantes
+    [SerializeField] const int INITIAL_HEALTH = 100;
+    [SerializeField] const int MAX_HEALTH = 100;
+    #endregion
 
     #region Variables del movimiento del jugador
-        [SerializeField] float jumpForce = 6f;
+    [SerializeField] float jumpForce = 6f;
         [SerializeField] float runningSpeed = 2f;
         Rigidbody2D rigidBody;
     #endregion
+
+    #region Variables de stats del jugador
+    int healthPoints = INITIAL_HEALTH;
+    #endregion
+
+    #region Variables para disparo de jugador
+    [SerializeField] Camera playerCamera; //Camara que nos otorgara la posición en la que apunta y dispara el usuario
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform aim; //Mira del jugador
+    Vector2 facingDirection;
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,10 +40,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        #region Movimiento
         if (Input.GetButtonDown("Jump"))
         {
             Jump();
         }
+
+        #endregion
+
+        #region Disparo
+        facingDirection = playerCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        aim.position = (Vector3)facingDirection.normalized + transform.position;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            float angle = Mathf.Atan2(facingDirection.y, facingDirection.x) * Mathf.Rad2Deg;
+            Quaternion rotationTarget = Quaternion.AngleAxis(angle, Vector3.forward);
+            GameObject newBullet = Instantiate(bullet, transform.position, rotationTarget);
+        }
+
+        #endregion
     }
 
     #region Métodos de movimiento del jugador
@@ -51,5 +83,21 @@ public class PlayerController : MonoBehaviour
 
     #endregion
 
+    #region Métodos de estadisticas del jugador
+    public void CollectHealth(int points)
+    {
+        if (healthPoints + points <= 0)
+            //Die();
+        if (healthPoints + points <= MAX_HEALTH)
+        {
+            healthPoints += points;
+        }
+        else
+        {
+            healthPoints = MAX_HEALTH;
+        }
+    }
 
+
+    #endregion
 }
