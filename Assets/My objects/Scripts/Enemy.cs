c3d1506a3605fdfe4b9dd5a6615c863cf9f47fb5
Daniel H <daniel_hernandez130999@hotmail.com>
2022-11-21 22:28:18 -0600

    Enemigo patrulla plataformas

--- Assets/My objects/Scripts/Enemy.cs
+++ Assets/My objects/Scripts/Enemy.cs
@@ -4,19 +4,40 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed = 5;
-    int damage = 10;
+    [SerializeField] float speed = 5;
+    [SerializeField] int damage = 10;
     [SerializeField] int health = 5;
+    bool facingRight = true;
+    Rigidbody2D enemyRigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (enemyRigidbody.velocity.x == 0)
+            facingRight = !facingRight;
+        PlatformPatrolling();
+    }
+
+
+    public void PlatformPatrolling()
+    {
+        float currentSpeed = speed;
+        if (facingRight)
+        {
+            this.transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else
+        {
+            currentSpeed = -speed;
+            this.transform.eulerAngles = Vector3.zero;
+        }
+
+        enemyRigidbody.velocity = new Vector2(currentSpeed, enemyRigidbody.velocity.y);
     }
 
     public void TakeDamage()
@@ -34,5 +55,6 @@ public class Enemy : MonoBehaviour
         {
             collision.GetComponent<PlayerController>().TakeDamage(damage);
         }
+        
     }
 }
