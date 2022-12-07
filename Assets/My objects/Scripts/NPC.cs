fe92cb7577307f48710e99bf814f54396b14683c
JuanGomez-jpg <juan.mojarro7920@alumnos.udg.mx>
2022-12-06 21:20:54 -0600

    NPC not working

---
+++ Assets/My objects/Scripts/NPC.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+
+    public GameObject dialoguePanel;
+    public Text dialogueText;
+    public string[] dialogue;
+    [SerializeField] private int index;
+    public GameObject contButton;
+    [SerializeField] public float wordSpeed = 0.008f;
+    [SerializeField] public bool playerIsClose;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (contButton == null)
+        {
+            Debug.Log("No existe el componente botón");
+        }
+        if (Input.GetKeyDown(KeyCode.F) && playerIsClose)
+        {
+            if (dialoguePanel.activeInHierarchy)
+            {
+                clearText();
+            }
+            else
+            {
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
+        }
+
+        if (dialogueText.text == dialogue[index])
+        {
+            contButton.SetActive(true);
+        }
+    }
+
+    public void clearText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+
+    public void Test()
+    {
+        Debug.Log("JALA MIERDA >:v");
+    }
+
+    IEnumerator Typing()
+    {
+        foreach(char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        Debug.Log("Si jala el boton");
+        contButton.SetActive(false);
+        if (index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            clearText();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerIsClose = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerIsClose = false;
+            clearText();
+        }
+    }
+}
